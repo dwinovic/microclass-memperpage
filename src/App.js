55380d2a470e700010a91b845a7ba3225ce0ed99
dwinovic <nd.cahya19@gmail.com>
2021-03-03 11:12:26 +0700

    initial commit and setup tailwindcss for react app project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto pt-2"> 
+      <h1 className="text-6xl">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
